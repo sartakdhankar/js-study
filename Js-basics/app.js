0448368e44097eb1c7b22dfb73cbb33923f253f4
sartakdhankar <145408777+sartakdhankar@users.noreply.github.com>
2025-07-30 15:00:30 +0530

    initial commit

--- Js-basics/app.js
+++ Js-basics/app.js
@@ -1,1 +1,9 @@
-alert('Hello world');
+let tshirt=10;
+let jeans=20;
+let socks=8;
+let totalCost = tshirt + jeans + socks;
+console.log("Total cost of items: $" + totalCost);
+
+document.body.innerHTML = '';
+
+document.head.innerHTML = '<title>My First JavaScript App</title>';

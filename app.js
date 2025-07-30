91852ad26df29b5ba237bd22524aae4948028804
sartakdhankar <145408777+sartakdhankar@users.noreply.github.com>
2025-07-30 14:53:43 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// exercise a.
+
+console.log(10 + (3*8)+5);
+
+// exercise b.
+
+console.log((10 + 3 * 8 + 5)/3);
+// exercise c.
+
+console.log(18.5 + (2*7.5));
+
+// exercise d.
+console.log((18.5 + 2 * 7.5) * 0.1);
+
+// exercise e.
+console.log((18.5 + 2 * 7.5) * 0.2);
